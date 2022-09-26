122f8a796195b245057b762ecf8b1f6564fe6dfa
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-25 23:08:50 -0300

    Jogo Da Velha WSForm

---
+++ exe3dsfelipe/prjJogoFelipe.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace exe3dsfelipe
+{
+	partial class prjJogoVelha
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnLinha1 = new System.Windows.Forms.Button();
+			this.btnLinha2 = new System.Windows.Forms.Button();
+			this.btnLinha3 = new System.Windows.Forms.Button();
+			this.btnLinha4 = new System.Windows.Forms.Button();
+			this.btnLinha5 = new System.Windows.Forms.Button();
+			this.btnLinha6 = new System.Windows.Forms.Button();
+			this.btnLinha7 = new System.Windows.Forms.Button();
+			this.btnLinha8 = new System.Windows.Forms.Button();
+			this.btnLinha9 = new System.Windows.Forms.Button();
+			this.lblJogador = new System.Windows.Forms.Label();
+			this.lblTitulo = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnLinha1
+			// 
+			this.btnLinha1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnLinha1.Location = new System.Drawing.Point(262, 177);
+			this.btnLinha1.Name = "btnLinha1";
+			this.btnLinha1.Size = new System.Drawing.Size(59, 49);
+			this.btnLinha1.TabIndex = 1;
+			this.btnLinha1.UseVisualStyleBackColor = true;
+			// 
+			// btnLinha2
+			// 
+			this.btnLinha2.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnLinha2.Location = new System.Drawing.Point(357, 176);
+			this.btnLinha2.Name = "btnLinha2";
+			this.btnLinha2.Size = new System.Drawing.Size(59, 50);
+			this.btnLinha2.TabIndex = 2;
+			this.btnLinha2.UseVisualStyleBackColor = true;
+			// 
+			// btnLinha3
+			// 
+			this.btnLinha3.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnLinha3.Location = new System.Drawing.Point(448, 177);
+			this.btnLinha3.Name = "btnLinha3";
+			this.btnLinha3.Size = new System.Drawing.Size(59, 50);
+			this.btnLinha3.TabIndex = 3;
+			this.btnLinha3.UseVisualStyleBackColor = true;
+			// 
+			// btnLinha4
+			// 
+			this.btnLinha4.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnLinha4.Location = new System.Drawing.Point(262, 252);
+			this.btnLinha4.Name = "btnLinha4";
+			this.btnLinha4.Size = new System.Drawing.Size(59, 49);
+			this.btnLinha4.TabIndex = 4;
+			this.btnLinha4.UseVisualStyleBackColor = true;
+			// 
+			// btnLinha5
+			// 
+			this.btnLinha5.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnLinha5.Location = new System.Drawing.Point(357, 252);
+			this.btnLinha5.Name = "btnLinha5";
+			this.btnLinha5.Size = new System.Drawing.Size(59, 49);
+			this.btnLinha5.TabIndex = 5;
+			this.btnLinha5.UseVisualStyleBackColor = true;
+			// 
+			// btnLinha6
+			// 
+			this.btnLinha6.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnLinha6.Location = new System.Drawing.Point(448, 252);
+			this.btnLinha6.Name = "btnLinha6";
+			this.btnLinha6.Size = new System.Drawing.Size(59, 49);
+			this.btnLinha6.TabIndex = 6;
+			this.btnLinha6.UseVisualStyleBackColor = true;
+			// 
+			// btnLinha7
+			// 
+			this.btnLinha7.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnLinha7.Location = new System.Drawing.Point(262, 318);
+			this.btnLinha7.Name = "btnLinha7";
+			this.btnLinha7.Size = new System.Drawing.Size(59, 49);
+			this.btnLinha7.TabIndex = 7;
+			this.btnLinha7.UseVisualStyleBackColor = true;
+			// 
+			// btnLinha8
+			// 
+			this.btnLinha8.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnLinha8.Location = new System.Drawing.Point(357, 318);
+			this.btnLinha8.Name = "btnLinha8";
+			this.btnLinha8.Size = new System.Drawing.Size(59, 49);
+			this.btnLinha8.TabIndex = 8;
+			this.btnLinha8.UseVisualStyleBackColor = true;
+			// 
+			// btnLinha9
+			// 
+			this.btnLinha9.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnLinha9.Location = new System.Drawing.Point(448, 318);
+			this.btnLinha9.Name = "btnLinha9";
+			this.btnLinha9.Size = new System.Drawing.Size(59, 49);
+			this.btnLinha9.TabIndex = 9;
+			this.btnLinha9.UseVisualStyleBackColor = true;
+			// 
+			// lblJogador
+			// 
+			this.lblJogador.Font = new System.Drawing.Font("Arial Narrow", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblJogador.Location = new System.Drawing.Point(256, 107);
+			this.lblJogador.Name = "lblJogador";
+			this.lblJogador.Size = new System.Drawing.Size(218, 36);
+			this.lblJogador.TabIndex = 10;
+			this.lblJogador.Text = "X, sua vez!";
+			this.lblJogador.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// lblTitulo
+			// 
+			this.lblTitulo.AutoSize = true;
+			this.lblTitulo.Font = new System.Drawing.Font("Arial Narrow", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitulo.Location = new System.Drawing.Point(255, 9);
+			this.lblTitulo.Name = "lblTitulo";
+			this.lblTitulo.Size = new System.Drawing.Size(286, 42);
+			this.lblTitulo.TabIndex = 11;
+			this.lblTitulo.Text = "JOGO DA VELHA!!!";
+			// 
+			// prjJogoVelha
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = global::exe3dsfelipe.Properties.Resources.etecStudios;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.lblTitulo);
+			this.Controls.Add(this.lblJogador);
+			this.Controls.Add(this.btnLinha9);
+			this.Controls.Add(this.btnLinha8);
+			this.Controls.Add(this.btnLinha7);
+			this.Controls.Add(this.btnLinha6);
+			this.Controls.Add(this.btnLinha5);
+			this.Controls.Add(this.btnLinha4);
+			this.Controls.Add(this.btnLinha3);
+			this.Controls.Add(this.btnLinha2);
+			this.Controls.Add(this.btnLinha1);
+			this.Name = "prjJogoVelha";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "JOGO DA VELHA FELIPE1501";
+			this.Load += new System.EventHandler(this.prjJogoVelha_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button btnLinha1;
+		private System.Windows.Forms.Button btnLinha2;
+		private System.Windows.Forms.Button btnLinha3;
+		private System.Windows.Forms.Button btnLinha4;
+		private System.Windows.Forms.Button btnLinha5;
+		private System.Windows.Forms.Button btnLinha6;
+		private System.Windows.Forms.Button btnLinha7;
+		private System.Windows.Forms.Button btnLinha8;
+		private System.Windows.Forms.Button btnLinha9;
+		private System.Windows.Forms.Label lblJogador;
+		private System.Windows.Forms.Label lblTitulo;
+	}
+}
+
---
+++ exe3dsfelipe/prjJogoFelipe.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exe3dsfelipe
+{
+	public partial class prjJogoVelha : Form
+	{
+		bool xeis = true;
+		public prjJogoVelha()
+		{
+			InitializeComponent();
+		}
+
+
+		private void prjJogoVelha_Load(object sender, EventArgs e)
+		{
+			btnLinha1.Click += new EventHandler(VClick);
+			btnLinha2.Click += new EventHandler(VClick);
+			btnLinha3.Click += new EventHandler(VClick);
+			btnLinha4.Click += new EventHandler(VClick);
+			btnLinha5.Click += new EventHandler(VClick);
+			btnLinha6.Click += new EventHandler(VClick);
+			btnLinha7.Click += new EventHandler(VClick);
+			btnLinha8.Click += new EventHandler(VClick);
+			btnLinha9.Click += new EventHandler(VClick);
+
+			foreach (Control item in this.Controls)
+			{
+				if (item is Button)
+				{
+					item.TabStop = false;
+				}
+			}
+		}
+
+		private void VClick(object sender, EventArgs e)
+		{
+			((Button)sender).Text = this.xeis ? "X" : "O";
+			((Button)sender).Enabled = false;
+			verificarGanhador();
+
+			xeis = !xeis;
+
+			lblJogador.Text = String.Format("{0}, sua vez!", this.xeis ? "X" : "O");
+		}
+		private void verificarGanhador()
+		{
+			if (btnLinha1.Text != string.Empty && btnLinha1.Text == btnLinha2.Text && btnLinha2.Text == btnLinha3.Text ||
+			btnLinha4.Text != string.Empty && btnLinha4.Text == btnLinha5.Text && btnLinha5.Text == btnLinha6.Text ||
+			btnLinha7.Text != string.Empty && btnLinha7.Text == btnLinha8.Text && btnLinha8.Text == btnLinha9.Text ||
+
+			btnLinha1.Text != string.Empty && btnLinha1.Text == btnLinha4.Text && btnLinha4.Text == btnLinha7.Text ||
+			btnLinha2.Text != string.Empty && btnLinha2.Text == btnLinha5.Text && btnLinha5.Text == btnLinha8.Text ||
+			btnLinha3.Text != string.Empty && btnLinha3.Text == btnLinha6.Text && btnLinha6.Text == btnLinha9.Text ||
+
+			btnLinha1.Text != string.Empty && btnLinha1.Text == btnLinha5.Text && btnLinha5.Text == btnLinha9.Text ||
+			btnLinha3.Text != string.Empty && btnLinha3.Text == btnLinha5.Text && btnLinha5.Text == btnLinha7.Text)
+			{
+				MessageBox.Show(string.Format("O GANHADOR FOI O JOGADOR {0}!!!", xeis ? "X" : "O"), "TEMOS UM VENCEDOR HOMO SAPIENS!!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				Resetar();
+			}
+			else
+			{
+				verificarEmpate();
+			}
+		}
+		private void verificarEmpate()
+		{
+			bool tudoCheio = true;
+
+			foreach (Control item in this.Controls)
+			{
+				if (item is Button && item.Enabled)
+				{
+					tudoCheio = false;
+					break;
+				}
+			}
+			if (tudoCheio)
+			{
+				MessageBox.Show(string.Format("POXA, DEU EMPATE!!!"), "NÃO ACREDITO!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+				Resetar();
+			}
+		}
+
+		private void Resetar()
+		{
+			foreach (Control item in this.Controls)
+			{
+				if (item is Button)
+				{
+					item.Enabled = true;
+					item.Text = string.Empty;
+				}
+			}
+		}
+	}
+}
